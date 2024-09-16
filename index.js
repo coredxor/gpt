5413cba943770c5a024c8926fc09e2c07cfc32d4
coredxor <118900553+coredxor@users.noreply.github.com>
2024-09-16 14:19:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
